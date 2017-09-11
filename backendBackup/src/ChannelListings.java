405164af55bbb7d28b6c45e30639188cef302299
Ayodeji Marquis <ayodejimarquis@gmail.com>
2017-09-11 01:01:37 -0400

    Backend Cleanup S/O peter

---
+++ backendBackup/src/ChannelListings.java
@@ -0,0 +1,12 @@
+public class ChannelListings {
+    private String[] listing;
+    private String[] time;
+
+    public ChannelListings(String[] listing, String[] time) {
+        this.listing = listing;
+        this.time = time;
+    }
+
+    public ChannelListings() {
+    }
+}
